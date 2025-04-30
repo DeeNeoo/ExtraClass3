1dc735ebb5066845a9683b631e26a6cfac1035b8
DeeNeoo <ST10490306@imconnect.edu.za>
2025-04-30 15:11:32 +0200

    first commit

---
+++ Pizza/app/src/main/java/za/ac/iie/pizza/MainActivity.kt
@@ -0,0 +1,79 @@
+package za.ac.iie.pizza
+
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.RadioButton
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val rbSmall = findViewById<RadioButton>(R.id.rbSmall)
+        val rbMedium  = findViewById<RadioButton>(R.id.rbMedium)
+        val rbLarge = findViewById<RadioButton>(R.id.rbLarge)
+        val chkOlives =findViewById<CheckBox>(R.id.chkOlives)
+        val chkCheese= findViewById<CheckBox>(R.id.chkCheese)
+        val btnCalculate = findViewById<Button>(R.id.btnCalculate)
+        val btnClear = findViewById<Button>(R.id.btnClear)
+        val txtResults= findViewById<TextView>(R.id.txtResults)
+
+        btnCalculate.setOnClickListener {
+            var total = 0.0
+
+
+            if(rbSmall.isChecked)
+            {
+                total = total + 59.99
+            }
+            else if(rbMedium.isChecked)
+            {
+                total += 89.99
+            }
+            else if(rbLarge.isChecked)
+            {
+                total = total + 119.99
+            }
+
+            if (chkOlives.isChecked)
+            {
+                total = total + 5.50
+            }
+
+            if (chkCheese.isChecked)
+            {
+                total = total + 3.50
+            }
+            txtResults.text = String.format("Total Amount Due: R%.2f",total)
+        }
+
+        btnClear.setOnClickListener {
+            txtResults.text = ""
+            rbSmall.isChecked = false
+            rbMedium.isChecked = false
+            rbLarge.isChecked = false
+            chkCheese.isChecked = false
+            chkOlives.isChecked = false
+        }
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars  = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+
+
+        }
+
+    }
+}
